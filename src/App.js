dccd8afc24468f761552445d0840e533325fd555
FAMMMI <naimfami@gmail.com>
2022-05-17 19:25:35 +0600

    admin panel perfectly added and doctor adding realated programming set

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import MyReview from './Pages/Dashboard/MyReview';
 import MyHistory from './Pages/Dashboard/MyHistory';
 import Users from './Pages/Dashboard/Users';
 import RequireAdmin from './Pages/Login/RequireAdmin';
+import AddDoctor from './Pages/Dashboard/AddDoctor';
+import ManageDoctors from './Pages/Dashboard/ManageDoctors';
 
 
 function App() {
@@ -40,6 +42,12 @@ function App() {
           <Route path='history' element={<MyHistory></MyHistory>}></Route>
           <Route path='users' element={<RequireAdmin>
             <Users></Users>
+          </RequireAdmin>}></Route>
+          <Route path='addDoctor' element={<RequireAdmin>
+            <AddDoctor></AddDoctor>
+          </RequireAdmin>}></Route>
+          <Route path='manageDoctors' element={<RequireAdmin>
+            <ManageDoctors></ManageDoctors>
           </RequireAdmin>}></Route>
         </Route>
 
